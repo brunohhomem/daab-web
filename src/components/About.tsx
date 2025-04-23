2675b3d8b21851f7ca575fe4811b6769c463b720
Bruno H. Homem <brunohhomem@gmail.com>
2025-04-23 15:38:37 -0300

    feat: add about

---
+++ src/components/About.tsx
@@ -0,0 +1,14 @@
+export default function About() {
+  return (
+    <section
+      id="about"
+      className="flex flex-col items-center mb-28 max-w-[50rem] sm:mb-0 scroll-mt-[100rem]"
+    >
+      <div className="m-20"></div>
+      <h1 className="text-zinc-700 text-2xl font-bold tracking-tight mb-4 text-center">
+        DEBORAH APARECIDA ASSAD BAZO: EQUIPE DE PERITOS e ASSISTENTES TÉCNICOS À
+        SUA DISPOSIÇÃO
+      </h1>
+    </section>
+  )
+}
