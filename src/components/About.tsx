306acb220c75838b97ace31ae4700d9b6fe0fdbe
Bruno H. Homem <brunohhomem@gmail.com>
2025-04-24 08:59:01 -0300

    Feat: added About

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -5,10 +5,20 @@ export default function About() {
       className="flex flex-col items-center mb-28 max-w-[50rem] sm:mb-0 scroll-mt-[100rem]"
     >
       <div className="m-20"></div>
-      <h1 className="text-zinc-700 text-2xl font-bold tracking-tight mb-4 text-center">
-        DEBORAH APARECIDA ASSAD BAZO: EQUIPE DE PERITOS e ASSISTENTES TÉCNICOS À
-        SUA DISPOSIÇÃO
-      </h1>
+      <h2 className="text-zinc-700 text-2xl font-bold tracking-tight mb-4 text-center">
+        Nossa História
+      </h2>
+      <p>
+        Em 20 anos de carreira acadêmica e prática, Deborah Aparecida Assad Bazo
+        construiu uma trajetória marcada pela busca incessante da verdade
+        técnica. Formada em Letras e com especialização em Grafoscopia pela USP,
+        ela uniu sua paixão por línguas e investigação para fundar a DAAB
+        Perícias. Cada laudo é fruto de métodos validados, equipamentos de
+        última geração e uma equipe multidisciplinar. Com experiência em
+        ambientes judiciais e corporativos, nossa missão é traduzir evidências
+        em respostas claras, protegendo direitos individuais e garantindo
+        decisões justas.
+      </p>
     </section>
   )
 }
